804c4b26c8dc0a35dc25024388094cc004eae943
LeeSB123 <lsb1896@naver.com>
2022-06-09 01:25:07 +0900

    test2

--- src/codeup100/c1079.java
+++ src/codeup100/c1079.java
@@ -1,13 +1,7 @@
 package codeup100;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class c1079 {
 	public static void main(String[] args) {
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		
 		System.out.println("Hello world");
 	}
 }
