f7db0bcfd2b94a21e5f96fea5b1e33443bf2d0c1
LeeSB123 <lsb1896@naver.com>
2022-06-09 01:28:25 +0900

    change

---
+++ src/c1079/c1079.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class c1079 {
+	public static void main(String[] args) {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
--- src/codeup100/c1079.java
+++
@@ -1,7 +0,0 @@
-package codeup100;
-
-public class c1079 {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-}
