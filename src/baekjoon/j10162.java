998603f01d858a09ce3f98ddbacc8ca6e0d655d7
LeeSB123 <lsb1896@naver.com>
2022-06-30 02:57:53 +0900

    remove .git file

---
+++ src/baekjoon/j10162.java
@@ -0,0 +1,65 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class j10162 {
+	public static void main(String[] args) throws IOException {
+//	memory 18728kb runtime 244ms
+//		Scanner scan = new Scanner(System.in);
+//		int t = scan.nextInt();
+//		
+//		int a = 0;
+//		int b = 0;
+//		int c = 0;
+//		
+//		if (t >= 300) {
+//			a = t / 300;
+//			t -= 300 * a;
+//		}
+//		if (t >= 60) {
+//			b = t / 60;
+//			t -= 60 * b;
+//		}
+//		if (t % 10 != 0) {
+//			System.out.println("-1");
+//			System.exit(0); // 강제종료 
+//		}
+//		else {
+//			c = t / 10;
+//			System.out.println(a +" "+ b + " " + c);
+//		}
+//		
+//		scan.close();
+
+//	memory 16352kb runtime 180ms
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(reader.readLine());
+		
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		
+		if (t >= 300 ) {
+			a = t / 300;
+			t -= 300 * a;
+		}
+		if (t >= 60) {
+			b = t / 60;
+			t -= 60 * b;
+		}
+		if (t % 10 != 0) {
+			System.out.println("-1");
+			System.exit(0);
+		}
+		else {
+			c = t / 10;
+			System.out.println(a + " " + b + " " + c);
+		}
+		reader.close();
+	}
+}
---
+++ src/baekjoon/j2720.java
@@ -0,0 +1,63 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class j2720 {
+	public static void main(String[] args) throws IOException {
+//	memory 19396kb runtime 320ms
+//		Scanner scan = new Scanner(System.in);
+//		int count = scan.nextInt();
+//
+//		while (count != 0) {
+//			int m = scan.nextInt();
+//			
+//			int q = m / 25;
+//			int d = (m - q * 25) / 10;
+//			int n = (m - q * 25 - d * 10) / 5; 
+//			int p = m - q * 25 - d * 10 - n * 5;
+//			System.out.println(q + " " + d + " " + n + " " + p);
+//			count--;
+//		}
+//		scan.close();
+		
+//	memory 16412kb runtime 220ms
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//		int count = Integer.parseInt(reader.readLine());
+//		
+//		while (count != 0) {
+//			int m = Integer.parseInt(reader.readLine());
+//			
+//			int q = m / 25;
+//			int d = (m - q * 25) / 10;
+//			int n = (m - q * 25 - d * 10) / 5;
+//			int p = m - q * 25 - d * 10 - n * 5;
+//			System.out.println(q + " " + d + " " + n + " " + p);
+//			count--;
+//		}
+//		reader.close();
+		
+//	memory 16328kb runtime 168ms
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int count = Integer.parseInt(reader.readLine());
+		
+		while (count != 0) {
+			int m = Integer.parseInt(reader.readLine());
+			
+			int q = m / 25;
+			int d = (m - q * 25) / 10;
+			int n = (m - q * 25 - d * 10) / 5;
+			int p = m - q * 25 - d * 10 - n * 5;
+			writer.write(q + " " + d + " " + n + " " + p + "\n");
+			count--;
+		}
+		reader.close();
+		writer.flush();
+		writer.close();
+	}
+}
