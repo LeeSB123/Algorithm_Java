69ae4441640b44fca03ea94451b6e473fd325b82
LeeSB123 <lsb1896@naver.com>
2022-07-08 06:43:06 +0900

    baekjoon j2930

--- src/baekjoon/j2930.java
+++ src/baekjoon/j2930.java
@@ -7,30 +7,78 @@ public class j2930 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		int R = scan.nextInt();
-		int N = 0;
-		String sangeun[] = new String[R];
-		String count[] = new String[N];
 		
-		for (int i = 0 ; i < R ; i++) {
-			sangeun[i] = scan.next();
+		int rnd = scan.nextInt(); 		// round
+		char user[] = new char[rnd];	// 상근이 주먹가위보
+		int score = 0;					// 상근이 점수
+		
+		for (int i = 0 ; i < rnd ; i++) {
+			user[i] = scan.next().charAt(0);
 		}
-		N = scan.nextInt();
 		
-		for (int i = 0 ; i < N ; i++) {
-			for (int j = 0 ; j < R ; j++) {
-				count[j] = scan.next();
+		int cnt = scan.nextInt();
+		char friends[][] = new char[cnt][rnd];
+		
+		for (int i = 0 ; i < cnt ; i++) {
+			for (int j = 0 ; j < rnd ; j++) {
+				friends[i][j] = scan.next().charAt(0);	
 				
+				if (user[j] == 'S') {
+					if (friends[i][j] == 'P') {
+						score += 2;
+					} else if (friends[i][j] == 'R') {
+						score += 0;
+					} else {
+						score += 1;
+					}
+				}
+				if (user[j] == 'P') {
+					if (friends[i][j] == 'R') {
+						score += 2;
+					} else if (friends[i][j] == 'S') {
+						score += 0;
+					} else {
+						score += 1;
+					}
+				}
+				if (user[j] == 'R') {
+					if (friends[i][j] == 'S') {
+						score += 2;
+					} else if (friends[i][j] == 'P') {
+						score += 0;
+					} else {
+						score += 1;
+					}
+				}
 			}
-			System.out.print("\n");
 		}
 		
-		for (int i = 0 ; i < N ; i++) {
-			for (int j = 0 ; j < R ; j++) {
-				
-			}
-			
-		}
+		int max_score = 0;				// 상근이 최대점수 
 		
+		for (int i = 0 ; i < rnd ; i++) {
+			int spr[] = {0, 0, 0};			// 0 = 가위, 1 = 보, 2 = 바위
+			for (int j = 0 ; j < cnt ; j++) {
+				if (friends[j][i] == 'S') {
+					spr[0] += 1;
+					spr[1] += 2;
+					spr[2] += 0;
+				}
+				if (friends[j][i] == 'P') {
+					spr[0] += 0;
+					spr[1] += 1;
+					spr[2] += 2;
+				}
+				if (friends[j][i] == 'R') {
+					spr[0] += 2;
+					spr[1] += 0;
+					spr[2] += 1;
+				}
+			}
+			max_score += Math.max(spr[0], Math.max(spr[1], spr[2]));
+		}
+		System.out.println(score);
+		System.out.println(max_score);
+		
+		scan.close();
 	}
 }
