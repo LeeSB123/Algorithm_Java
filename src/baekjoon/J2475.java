39618768b8ff8ef1848e4febe7cb80b6d26d14d0
kongkang331 <lsb1896@naver.com>
2022-07-30 01:14:34 +0900

    Create J2475.java file

--- src/baekjoon/J2475.java
+++ src/baekjoon/J2475.java
@@ -1,10 +1,23 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class J2475 {
 
 	public static void main(String...args) {
 		
+		Scanner scan = new Scanner(System.in);
 		
+		int[] data = new int[5];
+		int sum = 0;
+		
+		for (int i = 0 ; i < data.length ; i++) {
+			data[i] = scan.nextInt();
+			data[i] *= data[i];
+			sum += data[i];
+		}
+		
+		System.out.println(sum % 10);
 		
 	}
 	
